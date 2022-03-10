591c95be64b19bbfe2a83cd7e2ad30b6988ebc58
danilmoiseenkov <danil.moiseenkoff@gmail.com>
2022-03-10 20:22:13 +0200

    added a possibility to set city

--- bot_app/commands/set_city.py
+++ bot_app/commands/set_city.py
@@ -3,9 +3,24 @@ from telegram.ext import CallbackContext
 
 from bot_app import messages
 
+import os, django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cafe_bot.settings")
+django.setup()
+
+from bot_app.models import Users, Cities
+
 
 def set_city(update: Update, context: CallbackContext, payload):
     query = update.callback_query
+
+    user = Users.objects.get(chat_id=query.message.chat_id)
+
+    city = Cities.objects.get(name=payload[0])
+
+    user.city_id = city
+
+    user.save()
 
     query.delete_message()
 
